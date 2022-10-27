0781555cb43d9cd74716a4e67e8e4c8e66b467a7
Veera Venkata Satyanarayana Telu <satya@opsmx.io>
2022-10-27 10:17:52 +0530

    Update index.tsx

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -201,7 +201,7 @@ export const Extension = (props: {
   resource: State;
 }) => {
   const ro = parseInfoFromResourceNode(props.tree, props.resource);
-  {console.log(ro)};
+  {console.log(props)};
   return <>
     <div>HELLLO ARGO PLUGIN TEST</div>
     <RolloutWidget rollout={ro} />;
